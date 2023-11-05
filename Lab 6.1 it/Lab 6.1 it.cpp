1c931371c61fae31fca76ef6ea256e726695892f
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-11-05 20:01:30 +0200

    Add project files.

---
+++ Lab 6.1 it/Lab 6.1 it.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <cmath>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+void printArray(int* arr, int size);
+void generateArray(int* arr, int size, int min, int max);
+int countElements(int* arr, int size);
+int sumElements(int* arr, int size);
+void replaceElements(int* arr, int size);
+
+int main() {
+	const int arrSize = 22;
+	int a[arrSize];
+
+	generateArray(a, arrSize, -10, 35);
+
+	cout << "Original array: ";
+	printArray(a, arrSize);
+
+
+	cout << "Number of elemnts: " << countElements(a, arrSize) << endl;
+
+	cout << "Sum of elements: " << sumElements(a, arrSize) << endl;
+
+	replaceElements(a, arrSize);
+
+	cout << "Modified:       ";
+	printArray(a, arrSize);
+
+	return 0;
+}
+
+void generateArray(int* arr, int size, int min, int max) {
+	srand(unsigned(time(NULL)));
+	for (int i = 0; i < size; i++) {
+		arr[i] = min + rand() % (max - min + 1);
+	}
+}
+void printArray(int* array, int size) {
+	for (int i = 0; i < size; i++) {
+		cout << setw(4) << array[i] << " ";
+	}
+	cout << endl;
+}
+
+int sumElements(int* arr, int size) {
+
+	int sum = 0;
+	for (int i = 0; i < size; i++) {
+		if (arr[i] > 0 || (arr[i] % 3 != 0))
+		{
+			sum += arr[i];
+		};
+	}
+	return sum;
+}
+
+int countElements(int* arr, int size) {
+	int count = 0;
+	for (int k = 0; k < size; k++) {
+		if (arr[k] > 0 || (arr[k] % 3 != 0)) {
+			count++;
+		};
+	}
+	return count;
+}
+
+void replaceElements(int* arr, int size) {
+	for (int j = 0; j < size; j++) {
+		if (arr[j] > 0 || (arr[j] % 3 != 0)) {
+			arr[j] = 0;
+		}
+	}
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.1 it/Lab 6.1 it.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int test[22] = { 6, -10, 21, 2, 26, 10, 28, -1, 13, -6, 24,  -5, 30, 9, -1, 20,- 3, 26, 12, 14, 10, 28 };
+			int a;
+			a = countElements(test, 22);
+			Assert::AreEqual(a, 20);
+
+			a = sumElements(test, 22);
+			Assert::AreEqual(a, 262);
+		}
+	};
+}
